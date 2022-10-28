7657031f27663dfefcda2461101a64ed676b7ce9
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2022-10-28 10:22:05 -0500

    Final

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,36 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    
+    with open('clusters_report.txt') as archivo:
+        row = archivo.readlines()
 
+    row=row[4:]
+
+    clusters = []
+    cluster = [0, 0, 0, '']
+
+    for fila in row:
+        if re.match('^ +[0-9]+ +', fila):
+            number, quantity, percentage, *words = fila.split()
+            cluster[0] = int(number)
+            cluster[1] = int(quantity)
+            cluster[2] = float(percentage.replace(',','.')) 
+
+            words.pop(0) 
+            words = ' '.join(words) 
+            cluster[3] += words
+
+        elif re.match('^ +[a-z]', fila):
+            words = fila.split()
+            words = ' '.join(words) 
+            cluster[3] += ' ' + words 
+
+        elif re.match('^\n', fila) or re.match('^ +$', fila):
+            cluster[3] = cluster[3].replace('.', '') 
+            clusters.append(cluster) 
+            cluster = [0, 0, 0, ''] 
+
+    df = pd.DataFrame (clusters, columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+    
     return df
